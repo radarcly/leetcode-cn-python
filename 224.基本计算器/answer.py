0f25bab170096e96c67ba9fd489677c53e34cc1e
radarcly <16302010001@fudan.edu.cn>
2021-03-10 16:42:28 +0800

    2021.3.10

---
+++ 224.基本计算器/answer.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def calculate(self, s):
+        ops = []
+        ops.append(1)
+        sign = 1
+        ret = 0
+        i = 0
+        while i < len(s):
+            if s[i] == " ":
+                i += 1
+            elif s[i] == "+":
+                sign = ops[-1]
+                i += 1
+            elif s[i] == "-":
+                sign = -ops[-1]
+                i += 1
+            elif s[i] == "(":
+                ops.append(sign)
+                i += 1
+            elif s[i] == ")":
+                ops.pop()
+                i += 1
+            else:
+                num = 0
+                while i < len(s) and s[i].isdigit():
+                    num = num  * 10 + int(s[i])
+                    i += 1
+                ret += sign * num
+        return ret
